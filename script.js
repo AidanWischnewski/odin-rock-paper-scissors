438dd30f426472490ec4ed91df8d7ed720cb638f
Aidan <aidan.wischnewski@gmail.com>
2025-01-01 15:42:27 -0800

    added play round and play game

--- script.js
+++ script.js
@@ -1,32 +1,93 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-    let randomNumber = Math.random()
+    const randomNumber = Math.random()
     console.log(randomNumber)
     if(randomNumber < 1/3){
-        return "Rock!"
+        return "Rock"
     }
     else if(randomNumber <= 2/3){
-        return "Paper!"
+        return "Paper"
     }
     else if(randomNumber <= 1){
-        return "Scissors!"
+        return "Scissors"
     }
 }
 
 function getHumanChoice(){
-    let humanChoice = window.prompt("Type 'Rock', 'Paper' or 'Scissors'");
+    const humanChoice = window.prompt("Type 'Rock', 'Paper' or 'Scissors'");
     if(humanChoice =="Rock"){
-        return "Rock!"
+        return "Rock"
     }
     else if(humanChoice =="Paper"){
-        return "Paper!"
+        return "Paper"
     }
     else if(humanChoice =="Scissors"){
-        return "Scissors!"
+        return "Scissors"
     }
     else{
-        console.log("That is not a valid choice, try again")
+        alert.log("That is not a valid choice, try again")
     }
 
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+    let winner = "Nobody"
+    if(humanChoice==computerChoice){
+        winner = "Nobody"
+   }
+    else if(humanChoice=="Rock" && computerChoice=="Scissors"){
+        winner = "Human"
+   }
+    else if(humanChoice=="Paper" && computerChoice=="Rock"){
+       winner = "Human"
+   }
+    else if(humanChoice=="Scissors" && computerChoice=="Paper"){
+       winner = "Human"
+   }
+    else if(computerChoice=="Rock" && humanChoice=="Scissors"){
+        winner = "Computer"
+    }
+    else if(computerChoice=="Paper" && humanChoice=="Rock"){
+        winner = "Computer"
+    }
+    else if(computerChoice=="Scissors" && humanChoice=="Paper"){
+        winner = "Computer"
+        }
+    console.log(`${winner} Wins`)
+    return winner
+    }
+
+
+function incrementScore(winner){
+    switch(winner){
+        case "Human": humanScore+=1; break
+        case "Computer": computerScore+=1; break
+        case "Nobody": break
+    }
+}
+function playGame(){
+    for (let i=0; i<5 ;i++){
+        let humanChoice = getHumanChoice()
+        let computerChoice = getComputerChoice()
+        console.log(`Computer Plays ${computerChoice}`)
+        console.log(`Human Plays ${humanChoice}`)
+        let winner = playRound(humanChoice,computerChoice)
+        incrementScore(winner)
+
+        console.log(`Human Score: ${humanScore}`)
+        console.log(`Computer Score: ${computerScore}`)
+    }
+    if (computerScore==humanScore){
+        console.log("The Game is a tie")
+    }
+    else if(computerScore>humanScore){
+        console.log("Computer Wins")
+    }
+    else if(humanScore> computerScore){
+        console.log("Human Wins")
+    }
+    }
+
+playGame();
